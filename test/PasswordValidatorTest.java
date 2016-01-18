a3f4d8a2d3d8371da3c1ce45f48f654e2da9c6f2
Jordi Anguela <jordi.anguela@gmail.com>
2016-01-18 22:23:00 +0100

    create a test Setup

--- test/PasswordValidatorTest.java
+++ test/PasswordValidatorTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,33 +9,35 @@ import org.junit.Test;
  */
 public class PasswordValidatorTest {
 
+	private PasswordValidator validator;
+
+	@Before
+	public void setup() {
+		validator = new PasswordValidator();
+	}
+
 	@Test
 	public void returnsTrueWhenPasswordIsValid() {
-		PasswordValidator validator = new PasswordValidator();
 		assertTrue(validator.validate("Ab3456789"));
 	}
 
 	@Test
 	public void returnsFalseWhenPasswordHasLessThanNineCharacters() {
-		PasswordValidator validator = new PasswordValidator();
 		assertFalse(validator.validate("12345678"));
 	}
 
 	@Test
 	public void cannotBeNull() {
-		PasswordValidator validator = new PasswordValidator();
 		assertFalse(validator.validate(null));
 	}
 
 	@Test
 	public void returnsFalseWhenPasswordDoesntContainAnUpperCaseLetter() {
-		PasswordValidator validator = new PasswordValidator();
 		assertFalse(validator.validate("1b3456789"));
 	}
 
 	@Test
 	public void returnsFalseWhenPasswordDoesntContainALowerCaseLetter() {
-		PasswordValidator validator = new PasswordValidator();
 		assertFalse(validator.validate("A23456789"));
 	}
 
