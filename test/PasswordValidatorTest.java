1babfde235752c2e6f5440b1a6d1909ea53e5770
Jordi Anguela <jordi.anguela@gmail.com>
2016-01-19 11:54:19 +0100

    rename test names

--- test/PasswordValidatorTest.java
+++ test/PasswordValidatorTest.java
@@ -17,37 +17,37 @@ public class PasswordValidatorTest {
 	}
 
 	@Test
-	public void returnsTrueWhenPasswordIsValid() {
+	public void a_password_that_fulfills_the_rules_is_valid() {
 		assertTrue(validator.validate("Ab3456789_"));
 	}
 
 	@Test
-	public void returnsFalseWhenPasswordHasLessThanNineCharacters() {
+	public void a_password_with_less_than_nine_characters_is_not_valid() {
 		assertPasswordIsFalse("Ab34567_");
 	}
 
 	@Test
-	public void cannotBeNull() {
+	public void a_null_password_is_not_valid() {
 		assertPasswordIsFalse(null);
 	}
 
 	@Test
-	public void returnsFalseWhenPasswordDoesntContainAnUpperCaseLetter() {
+	public void a_password_without_an_upper_case_letter_is_not_valid() {
 		assertPasswordIsFalse("b3456789_");
 	}
 
 	@Test
-	public void returnsFalseWhenPasswordDoesntContainALowerCaseLetter() {
+	public void a_password_without_a_lower_case_letter_is_not_valid() {
 		assertPasswordIsFalse("A3456789_");
 	}
 
 	@Test
-	public void returnsFalseWhenPasswordDoesntContainANumber() {
+	public void a_password_without_a_number_is_not_valid() {
 		assertPasswordIsFalse("Abbbbbccccc_");
 	}
 
 	@Test
-	public void returnsFalseWhenPasswordDoesntContainAnUnderscore() {
+	public void a_password_without_an_underscore_is_not_valid() {
 		assertPasswordIsFalse("Ab3456789");
 	}
 
